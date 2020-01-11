985d394f87e347ee9a54f71dedbc0f8afa849d89
Dawid Adamus <adamus.dawid@protonmail.com>
2020-01-11 13:51:37 +0100

    added book info feature

--- src/frontend/src/Book.js
+++ src/frontend/src/Book.js
@@ -1,16 +1,36 @@
-import {Button} from "react-bootstrap";
+import {Button, Form, Modal, Table} from "react-bootstrap";
 import React, {useState} from "react";
-import {removeRequest} from "./SharedKernel/ApiRequest";
+import {getRequest, removeRequest} from "./SharedKernel/ApiRequest";
 import {notify} from "react-notify-toast/bin/notify";
 
 export default function Book(props) {
     const [removed, remove] = useState(false);
+    const [show, setShow] = useState(false);
+    const [bookInfo, setBookInfo] = useState({author:{}});
 
     const removeBook = () => {
         removeRequest('/books/' + props.id).then(() => {
             remove(true);
             notify.show('Usunięto!');
         });
+    };
+
+    const handleClose = () => {
+        setShow(false);
+    };
+
+    const getBookInfo = () => {
+        getRequest('books/' + props.id).then((response) => {
+            return response.json();
+        }).then((response) => {
+            setBookInfo(response);
+        });
+    };
+
+    const showBookInfo = () => {
+        setBookInfo({author:{}});
+        setShow(true);
+        getBookInfo();
     };
 
     if (removed) {
@@ -20,7 +40,43 @@ export default function Book(props) {
     return (
         <tr key={props.id}>
             <td>{props.lp}</td>
-            <td>{props.title}</td>
+            <td>
+                <a href="#" onClick={showBookInfo}>
+                    {props.title}
+                </a>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>{props.title}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Table bordered>
+                            <tbody>
+                            <tr>
+                                <td>Nazwa:</td>
+                                <td>{bookInfo.title}</td>
+                            </tr>
+                            <tr>
+                                <td>Wydawca:</td>
+                                <td>{bookInfo.publisher}</td>
+                            </tr>
+                            <tr>
+                                <td>Opis:</td>
+                                <td>{bookInfo.description}</td>
+                            </tr>
+                            <tr>
+                                <td>Autor:</td>
+                                <td>{bookInfo.author.name} {bookInfo.author.surname}</td>
+                            </tr>
+                            </tbody>
+                        </Table>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Zamknij
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </td>
             <td>{props.authorName} {props.authorSurname}</td>
             <td>
                 <Button
